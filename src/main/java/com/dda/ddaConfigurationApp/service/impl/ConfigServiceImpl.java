353e23e01635aec9abc801fb63e773c996a8df39
waelboussoffara@gmail.com <waelboussoffara@gmail.com>
2025-09-02 09:43:40 +0400

    add logs

--- src/main/java/com/dda/ddaConfigurationApp/service/impl/ConfigServiceImpl.java
+++ src/main/java/com/dda/ddaConfigurationApp/service/impl/ConfigServiceImpl.java
@@ -5,10 +5,12 @@ import com.dda.ddaConfigurationApp.service.ConfigService;
 import com.dda.ddaConfigurationApp.service.CoreService;
 import com.dda.ddaConfigurationApp.service.UIService;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ConfigServiceImpl implements ConfigService {
     @Autowired
     private CoreService coreService;
@@ -17,6 +19,7 @@ public class ConfigServiceImpl implements ConfigService {
     @Override
     @SneakyThrows
     public void createDDAProject(ConfigDDAProjectDto configDDAProjectDto) {
+        log.info("configDDAProjectDto={}", configDDAProjectDto);
         coreService.createProjectStructure(configDDAProjectDto);
          uiService.createProjectStructure(configDDAProjectDto);
 
